6e2af1a20a9cadf001126f9fd174547734dd09b9
Peter Joh <johpeter@gmail.com>
2022-03-12 15:40:58 -0700

    geolocation

--- react-app/src/components/map/Map.js
+++ react-app/src/components/map/Map.js
@@ -9,19 +9,47 @@ const mapStyles = {
 let key = process.env.REACT_APP_GOOGLEMAPS
 // console.log(key)
 
+let lat
+let long
+
+if(navigator.geolocation) {
+    console.log("navigator.geolocation is available");
+    navigator.geolocation.getCurrentPosition(function(position) {
+      console.log("current position acquired");
+      lat = Number(position.coords.latitude)
+      long = Number(position.coords.longitude)
+
+
+    });
+
+
+}
+
+
+
+
+
+
 export class MapContainer extends React.Component {
+
+
+
 
     render() {
         return (
+            <>
+
+            <div>{lat} {long}</div>
             <Map
                 google={this.props.google}
                 zoom={8}
                 style={mapStyles}
-                initialCenter={{ lat: 47.444, lng: -122.176 }}
+                initialCenter={{ lat: lat, lng: long }}
             >
                 <Marker position={{ lat: 48.00, lng: -122.00 }} />
                 <Marker position={{ lat: 39.00, lng: -120.00 }} />
             </Map>
+            </>
         );
     }
 }
