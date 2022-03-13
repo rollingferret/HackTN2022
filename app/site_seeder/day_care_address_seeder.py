ebd16724f3e3669213c1e847e9a280bb859fdaad
Alejandro C. Grant <authorcgrant@gmail.com>
2022-03-12 21:18:42 -0800

    addressed inexactitude of some addresses

--- app/site_seeder/day_care_address_seeder.py
+++ app/site_seeder/day_care_address_seeder.py
@@ -11,7 +11,7 @@ try:
     for day_care_name in day_cares:
         day_care = day_cares[day_care_name]
         geo_addy = app.geocode(day_care['address'], timeout=10)
-        location = geo_addy.raw if geo_addy else app.geocode(day_care['address'].split()[2]).raw 
+        location = geo_addy.raw if geo_addy else app.geocode(day_care['address'].split(', ')[1]).raw 
         day_care['coordinates'] = {'latitude': float(location['lat']), 'longitude': float(location['lon']), 'accuracy': 'exact' if geo_addy else 'inexact'}
         main_day_care_dictionary[day_care_name] = day_care
         print(f"Added geolocation data for: {day_care['name']}")
