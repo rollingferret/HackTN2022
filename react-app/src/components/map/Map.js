bbfdc3ee61487e5bc21d9217f41f76835209aa0f
Peter Joh <johpeter@gmail.com>
2022-03-12 18:18:35 -0700

    markers working

--- react-app/src/components/map/Map.js
+++ react-app/src/components/map/Map.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Map, GoogleApiWrapper, Marker, Circle } from 'google-maps-react';
+import { Map, GoogleApiWrapper, Marker, Circle } from 'google-maps-react';  
+import './map.css';
+import dayCareInfo from '../../site_data/formatted_daycares.json'
 
 const mapStyles = {
     width: '40%',
@@ -11,6 +13,8 @@ let key = process.env.REACT_APP_GOOGLEMAPS
 
 let lat = 36.1627
 let long = -86.7816
+
+console.log(dayCareInfo, 'check here')
 
 if(navigator.geolocation) {
     console.log("navigator.geolocation is available");
@@ -39,10 +43,10 @@ export class MapContainer extends React.Component {
 
     render() {
         return (
-            <>
 
+            <div>
             <div>{lat} {long}</div>
-            <Map
+            <Map className="mapContainer"
                 google={this.props.google}
                 zoom={13}
                 style={mapStyles}
@@ -62,12 +66,12 @@ export class MapContainer extends React.Component {
             />
                 {/* <Marker position={{ lat: 39.7275, lng: -104.8791 }} /> */}
                 {
-                    Object.values(coords).map((coords) => {
-                        return <Marker position={{ lat: coords[0], lng: coords[1]}}/>
+                    Object.values(dayCareInfo).map((dayCare) => {
+                        return <Marker position={{ lat: dayCare.coordinates.latitude, lng: dayCare.coordinates.longitude}}/>
                     })
                 }
             </Map>
-            </>
+            </div>
         );
     }
 }
